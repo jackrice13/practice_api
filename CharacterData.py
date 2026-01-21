7657476104e92d1db929bdd75c7c66348f3ac949
jackrice13 <jackrice13@gmail.com>
2026-01-21 18:22:57 -0500

    Updated Readme.md and .gitignore

---
+++ CharacterData.py
@@ -0,0 +1,23 @@
+import requests
+import pprint
+
+characterID=1303
+url = f"https://www.anapioficeandfire.com/api/characters/{characterID}"
+
+
+response = requests.get(url)
+data = response.json()
+# HouseName = (data["name"])
+# HouseWords = (data["words"])
+# HouseRegion = (data["region"])
+# HouseCoatOfArms = (data["coatOfArms"])
+
+#print(type(data))
+# print(HouseName)
+# print(HouseWords)
+pprint.pprint(data)
+
+# with open("HouseData.csv", "w") as f:
+#   f.write("House Name,House Words,House Region,House Coat Of Arms\n")
+#   f.write(f"{HouseName},{HouseWords},{HouseRegion},{HouseCoatOfArms}\n")
+
