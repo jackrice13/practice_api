d3c6d7b993736c83282293155081fc1ccf4bb4a6
jackrice13 <jackrice13@gmail.com>
2026-01-24 21:20:48 -0500

    Code works, Creates a CSV, with headers and adds a line each run of the file already exsists. File is created if it doesn't

--- GOTHouses.py
+++ GOTHouses.py
@@ -1,6 +1,6 @@
 import requests
-import pprint
 import CharacterData as char
+import os
 
 houseID=378
 url = f"https://www.anapioficeandfire.com/api/houses/{houseID}"
@@ -23,7 +23,22 @@ print(HLID)
 print(HouseLordName)
 #pprint.pprint(data)
 
-with open("HouseData.csv", "w") as f:
-  f.write("House Name,House Words,House Region,House Coat Of Arms, House Lord Name\n")
-  f.write(f"{HouseName},{HouseWords},{HouseRegion},{HouseCoatOfArms},{HouseLordName}\n")
+#creates new CSV
+file_exists = os.path.exists("HouseData.csv")
+print("with open")
+with open("HouseData.csv","a",encoding="utf-8",newline="") as f:
+  print("if not")
+  if not file_exists:
+    #writes headers if there is no file
+    print("→ Writing header")
+    f.write("House Name,House Words,House Region,House Coat Of Arms, House Lord Name\n")
+    print("→ Writing header")
+    #Writes new line each run
+  print("→ Writing data row")
+  f.write(f'{HouseName},{HouseWords},{HouseRegion},{HouseCoatOfArms},{HouseLordName}\n')
+  print("Write block finished")
+
+# with open("HouseData.csv", "w") as f:
+#   f.write("House Name,House Words,House Region,House Coat Of Arms, House Lord Name\n")
+#   f.write(f"{HouseName},{HouseWords},{HouseRegion},{HouseCoatOfArms},{HouseLordName}\n")
 
